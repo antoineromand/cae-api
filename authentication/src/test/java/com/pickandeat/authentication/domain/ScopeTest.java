dca1358e92db14172f95db638d1ceb9a61c9f179
Antoine Romand <antoine.romand@epitech.eu>
2025-05-22 00:45:57 +0200

    feat: create Scope class (domain layer) and unit test.

---
+++ authentication/src/test/java/com/pickandeat/authentication/domain/ScopeTest.java
@@ -0,0 +1,19 @@
+package com.pickandeat.authentication.domain;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ScopeTest {
+    @Test
+    public void shouldCreateAScope() {
+        Scope scope = new Scope(1, "read", "menu");
+        assertEquals("read:menu", scope.getScope());
+    }
+
+    @Test
+    public void shouldCreateAdminScope() {
+        Scope scope = new Scope(2, "*", "*");
+        assertTrue(scope.isAdmin());
+    }
+}
