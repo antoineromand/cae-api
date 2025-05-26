755e8ebd1cf1cefb8e6abf251e559b89991bd0f1
Antoine Romand <antoine.romand@epitech.eu>
2025-05-26 15:36:43 +0200

    feat: prepare register command, add fields to prepare the profile/user module

---
+++ authentication/src/main/java/com/pickandeat/authentication/application/usecase/RegisterCommand.java
@@ -0,0 +1,7 @@
+package com.pickandeat.authentication.application.usecase;
+
+import java.time.LocalDate;
+
+import com.pickandeat.authentication.domain.valueobject.Role;
+
+public record RegisterCommand(String email, String password, String firstName, String lastName, String phoneNumber, LocalDate birthDate, Role role) {}
